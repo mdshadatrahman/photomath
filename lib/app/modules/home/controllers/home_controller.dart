c0a7988a8b7dabfa0387b213b030488c7ac75c73
mdshadatrahman <shadat.rahman.464@gmail.com>
2023-02-25 18:10:55 +0600

    api fetch done

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,60 @@
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:developer' as developer show log;
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
   @override
-  void onInit() {
+  onInit() {
+    getDataFromPhotoMath();
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  getDataFromPhotoMath() async {
+    http.MultipartRequest request = http.MultipartRequest(
+      'POST',
+      Uri.parse('https://photomath1.p.rapidapi.com/maths/solve-problem'),
+    );
+    request.headers.addAll({
+      'X-RapidAPI-Key': '4007e7082bmsh46bff49b82fe990p1fbc57jsna6468573c702',
+      'X-RapidAPI-Host': 'photomath1.p.rapidapi.com',
+    });
 
-  @override
-  void onClose() {
-    super.onClose();
+    request.fields['locale'] = 'en';
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'image',
+        'assets/images/image1.jpeg',
+      ),
+    );
+    var response = await request.send();
+    final data = await response.stream.bytesToString();
+    developer.log(data.toString());
   }
-
-  void increment() => count.value++;
 }
+
+/*
+  const FormData = require("form-data");
+const fs = require("fs");
+const axios = require("axios");
+
+const data = new FormData();
+data.append("locale", "en");
+data.append("image", fs.createReadStream("/PATH/TO/image1.jpeg"));
+
+const options = {
+  method: 'POST',
+  url: 'https://photomath1.p.rapidapi.com/maths/solve-problem',
+  headers: {
+    'X-RapidAPI-Key': '4007e7082bmsh46bff49b82fe990p1fbc57jsna6468573c702',
+    'X-RapidAPI-Host': 'photomath1.p.rapidapi.com',
+    ...data.getHeaders()
+  },
+  data: data
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+*/
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,12 +8,13 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    controller.onInit();
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
+      body: const Center(
         child: Text(
           'HomeView is working',
           style: TextStyle(fontSize: 20),
